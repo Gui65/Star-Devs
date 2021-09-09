1e3cd604cc7d2c0e061c3924c7b705517af7f0ec
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2021-09-09 17:12:26 -0300

    inicializando o JS

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const person = document.getElementById('persons');
+const starship = document.getElementById('starships');
+const planets = document.getElementById('planets');
+
+fillCounters();
+
+function fillCounters(){
+    Promise.all([
+        getData('people/'),
+        getData('starships'),
+        getData('planets')
+    ])
+    .then(data => {
+        
+    })
+    .catch(err => console.log('Erro: ', err))
+}
+
+function getData(param){
+   return fetch(`https://swapi.dev/api/${param}`) 
+            .then(res => res.json())
+}
